cf38bc379bc788c05b1d8a6fb15aeff1f73dad3e
Your Name <your_email@example.com>
2023-04-13 13:08:01 -0500

    debug build

--- vite.config.ts
+++ vite.config.ts
@@ -38,16 +38,6 @@ export default defineConfig(({}) => {
         os: 'rollup-plugin-node-polyfills/polyfills/os',
         assert: 'rollup-plugin-node-polyfills/polyfills/assert',
         constants: 'rollup-plugin-node-polyfills/polyfills/constants',
-        _stream_duplex:
-            'rollup-plugin-node-polyfills/polyfills/readable-stream/duplex',
-        _stream_passthrough:
-            'rollup-plugin-node-polyfills/polyfills/readable-stream/passthrough',
-        _stream_readable:
-            'rollup-plugin-node-polyfills/polyfills/readable-stream/readable',
-        _stream_writable:
-            'rollup-plugin-node-polyfills/polyfills/readable-stream/writable',
-        _stream_transform:
-            'rollup-plugin-node-polyfills/polyfills/readable-stream/transform',
         timers: 'rollup-plugin-node-polyfills/polyfills/timers',
         console: 'rollup-plugin-node-polyfills/polyfills/console',
         vm: 'rollup-plugin-node-polyfills/polyfills/vm',
