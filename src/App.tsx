aaee946d4c51b04f8bc3c943777fb7f1365d90dd
Your Name <your_email@example.com>
2023-04-13 12:19:33 -0500

    debug build

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import { BrowserRouter as Router } from "react-router-dom";
 import { Web3OnboardProvider } from "@web3-onboard/react";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import { PioneerProvider } from "lib/context/Pioneer";
 import Layout from "lib/layout";
 import Routings from "lib/router/Routings";
 import { theme } from "lib/styles/theme";
-import web3Onboard from "./web3-onboard";
 
+import web3Onboard from "./web3-onboard";
+import * as buffer from "buffer";
+
+window.Buffer = buffer.Buffer;
 const App = () => (
-    <Web3OnboardProvider web3Onboard={web3Onboard}>
-  <ChakraProvider theme={theme}>
-    <Router>
-      <Layout>
-        <Routings />
-      </Layout>
-    </Router>
-  </ChakraProvider>
-    </Web3OnboardProvider>
+  <Web3OnboardProvider web3Onboard={web3Onboard}>
+    <PioneerProvider>
+      <ChakraProvider theme={theme}>
+        <Router>
+          <Layout>
+            <Routings />
+          </Layout>
+        </Router>
+      </ChakraProvider>
+    </PioneerProvider>
+  </Web3OnboardProvider>
 );
 
 export default App;
