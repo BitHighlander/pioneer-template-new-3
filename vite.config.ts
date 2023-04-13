2f4496016507ca4a3f3f06757688dea6f3ed1691
Your Name <your_email@example.com>
2023-04-13 12:44:37 -0500

    debug build

--- vite.config.ts
+++ vite.config.ts
@@ -19,15 +19,42 @@ export default defineConfig(({}) => {
       'process.env': {},
       global: {},
     },
-    plugins: [react(),
-      postcss({
+    plugins: [react(),postcss({
       // PostCSS plugins configuration
-      })
-    ],
+    })],
     resolve: {
       alias: {
         lib: resolve(__dirname, "src/lib"),
         routes: resolve(__dirname, "src/routes"),
+        // util: 'rollup-plugin-node-polyfills/polyfills/util',
+        sys: 'util',
+        events: 'rollup-plugin-node-polyfills/polyfills/events',
+        stream: 'rollup-plugin-node-polyfills/polyfills/stream',
+        path: 'rollup-plugin-node-polyfills/polyfills/path',
+        querystring: 'rollup-plugin-node-polyfills/polyfills/qs',
+        punycode: 'rollup-plugin-node-polyfills/polyfills/punycode',
+        url: 'rollup-plugin-node-polyfills/polyfills/url',
+        http: 'rollup-plugin-node-polyfills/polyfills/http',
+        https: 'rollup-plugin-node-polyfills/polyfills/http',
+        os: 'rollup-plugin-node-polyfills/polyfills/os',
+        assert: 'rollup-plugin-node-polyfills/polyfills/assert',
+        constants: 'rollup-plugin-node-polyfills/polyfills/constants',
+        _stream_duplex:
+            'rollup-plugin-node-polyfills/polyfills/readable-stream/duplex',
+        _stream_passthrough:
+            'rollup-plugin-node-polyfills/polyfills/readable-stream/passthrough',
+        _stream_readable:
+            'rollup-plugin-node-polyfills/polyfills/readable-stream/readable',
+        _stream_writable:
+            'rollup-plugin-node-polyfills/polyfills/readable-stream/writable',
+        _stream_transform:
+            'rollup-plugin-node-polyfills/polyfills/readable-stream/transform',
+        timers: 'rollup-plugin-node-polyfills/polyfills/timers',
+        console: 'rollup-plugin-node-polyfills/polyfills/console',
+        vm: 'rollup-plugin-node-polyfills/polyfills/vm',
+        zlib: 'rollup-plugin-node-polyfills/polyfills/zlib',
+        tty: 'rollup-plugin-node-polyfills/polyfills/tty',
+        domain: 'rollup-plugin-node-polyfills/polyfills/domain'
       },
     },
     optimizeDeps: {
@@ -51,7 +78,7 @@ export default defineConfig(({}) => {
       rollupOptions: {
         external: [
           /^node:.*/,
-          'lib/styles/theme'
+          ""
         ],
         plugins: [
           // inject({ Buffer: ['Buffer','Buffer'], process: ['process'] }),
