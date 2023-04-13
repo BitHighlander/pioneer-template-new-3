64aa235b7099b5408fb20de7a110f479b92139c7
Your Name <your_email@example.com>
2023-04-12 23:27:58 -0500

    onboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter as Router } from "react-router-dom";
-
+import { Web3OnboardProvider } from "@web3-onboard/react";
 import Layout from "lib/layout";
 import Routings from "lib/router/Routings";
 import { theme } from "lib/styles/theme";
+import web3Onboard from "./web3-onboard";
 
 const App = () => (
+    <Web3OnboardProvider web3Onboard={web3Onboard}>
   <ChakraProvider theme={theme}>
     <Router>
       <Layout>
@@ -13,6 +15,7 @@ const App = () => (
       </Layout>
     </Router>
   </ChakraProvider>
+    </Web3OnboardProvider>
 );
 
 export default App;
